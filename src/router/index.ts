8406c64def92c0724b7da832c53db29b49ae9d50
Samir Temtem <Samirtem06@gmail.com>
2022-11-20 18:31:37 +0100

    initial commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,38 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
-
+import formP from "../views/FormP.vue";
+import affichP from "../views/AffichageM.vue";
+import EtudiantP from '@/views/Etudiants.vue';
+import ModifierM from '@/views/ModifierM.vue';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/home'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
-]
+    path: '/home',
+    name: 'Formulaire',
+    component: formP,
+    props: true
+  },
+  {
+    path: '/affichage/:nom/:prenom/:civilite/:specialite/:Tmatiere*',
+    name: 'affichage',
+    component: affichP,
+    props: true
+  },
+  {
+    path: '/etudiants',
+    name: 'EtudiantP',
+    component: EtudiantP,
+    props: false
+  },
+  {
+    path: '/etudiants/:id',
+    name: 'modifier',
+    component: ModifierM,
+    props: true
+  }]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
